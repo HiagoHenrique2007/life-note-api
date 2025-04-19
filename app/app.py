7ffcd8e19943ca1f993e5ab1065c40a4dcc0bf7b
hiago <imhiagohenrique@gmail.com>
2025-04-19 17:57:28 -0300

    DATABASE: comecei a criar as tabelas, tenho que terminar

--- app/app.py
+++ app/app.py
@@ -6,16 +6,5 @@ app.include_router(customer)
 app.include_router(record.record_router)
 app.include_router(text.text_router)
 
-# @app.get('/records')
-# async def records(userId: int = Depends()):
-#   pass
 
-
-# @app.get('/record/by/emotion')
-# async def recordByEmotion():
-#   pass
-
-# @app.get('/record/by/feeling')
-# async def recordByFeeling():
-#   pass
 

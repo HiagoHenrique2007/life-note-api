2b75d04b6a754d2a63331d1efc60f122c68c527d
hiago <imhiagohenrique@gmail.com>
2025-04-19 15:56:52 -0300

    dependencia de verificar e retornar o id do cliente ja esta pronta

--- app/depends/customer_id.py
+++ app/depends/customer_id.py
@@ -2,6 +2,8 @@ from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError, ExpiredSignatureError
 
+SECRET_KEY = b'fIsH-_-BaLLCAT20comer70correr'
+ALGORITIHM = 'HS256'
 
 ''' o OAuthPasswordBearer é uma dependencia pronta que me retorna o token ja separado do header
   o primeiro parametro diz para o fastapi onde o usuario pode obter o token JWT
@@ -10,9 +12,13 @@ oauth2_schema = OAuth2PasswordBearer('login')
 
 async def getCustomerId(token: str = Depends(oauth2_schema)):
   try:
-    payload = jwt.decode(token, )
-
-  except ExpiredSignatureError:
-    HTTPException(401, detail='Token Expirado!')
-  except JWTError:
+    payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITIHM)
+    customer_id =  payload.get('sub', None)
+    if customer_id is None:
+      raise HTTPException(401, 'Token Invalido: claim sub ausente!')
+    return customer_id
+  
+  except ExpiredSignatureError: # exceção se o token estiver expirado
+    raise HTTPException(401, detail='Token Expirado!')
+  except JWTError: # exceção se o token foi auterado e quebrou a signature
     print('Token Adulterado!\tDanger!!!')
--- app/routes/record.py
+++ app/routes/record.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from app.depends.customer_id import getCustomerId
 
@@ -8,13 +8,19 @@ class By(BaseModel):
 record_router = APIRouter(prefix='/record')
 
 @record_router.get('/')
-async def getAllRecords(customer_id: int = Depends()):
+async def getAllRecords(customer_id: int = Depends(getCustomerId)):
   pass
 
 @record_router.get('/by/emotion')
-async def recordByEmotion(by: By, customer_id: int = Depends()):
+async def recordByEmotion(
+    by: By,
+    customer_id: int = Depends(getCustomerId)
+  ):
   pass
 
 @record_router.get('/by/feeling')
-async def recordByFeeling(by: By, customer_id: int = Depends()):
+async def recordByFeeling(
+    by: By,
+    customer_id: int = Depends(getCustomerId)
+  ):
   pass

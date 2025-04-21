b8bb2e13265708cf8115d04cbf7072163907fd6f
hiago <imhiagohenrique@gmail.com>
2025-04-21 12:52:58 -0300

    DB: fiz o decorador para criar o session e passar para os motodos do model, fiz o metodo para ver se o email ja existe no banco, para criar um customer e obter os dados dele

--- app/database/model.py
+++ app/database/model.py
@@ -4,33 +4,41 @@ from sqlalchemy import create_engine, select
 from sqlalchemy.orm import Session
 from functools import wraps
 
+engine = create_engine('sqlite:///app/database/feeling.db')
+Base.metadata.create_all(engine)
+
 def createSession(func):
+  print(f'Func: {func}') # executa somente essa parte
   @wraps(func)
   def wrapper(self, *args, **kwargs):
-    with Session(self.engine) as session:
-      return func(self, *args, session=session, **kwargs)
+    print(f'ARGS: {args}\n')
+    print(f'KWARGS: {kwargs}\n')
+    with Session(bind=engine) as session:
+      print(f'SESSION: {session}')
+      print(f'EXECUTANDO A FUNC')
+      result = func(self, *args, session=session, **kwargs)
+    return result
   return wrapper
 
 class Model:
-  def __init__(self):
-    self.engine = create_engine('sqlite:///app/database/feeling.db')
-    Base.metadata.create_all(self.engine)
+  def __init__(self): pass
 
   @createSession
-  def createCustomer(self, *, name: str, email: str, password: str, session: Session = None):
-    if not self.dontHasEmailOnDb(email): # se nao tiver o email vai retornar True, entao eu passo para False, que vai significar que ja existe usuario
+  def createCustomer(self, *, name: str, email: str, password: str, session: Session):
+    print(f'SESSION DENTRO DA FUNC: {session}')
+    if not self.dontHasEmailOnDb(email=email, session=session): # se nao tiver o email vai retornar True, entao eu passo para False, que vai significar que ja existe usuario
       raise HTTPException(409, detail='Usuario ja cadastrado!')
     customer = Customer(name=name, email=email, password=password)
     session.add(customer)
     session.commit()
     
-  def dontHasEmailOnDb(self, email: str) -> bool:
-    with Session(self.engine) as session:
-      stmt = select(Customer.email).where(Customer.email == email)
-      result = session.execute(stmt).scalars().first()
-      return result is None # eu estou verificando se nao tem o email, entao se o result for None, quer dizer que nao tem o email no banco, 
+  def dontHasEmailOnDb(self, email: str, session: Session) -> bool:
+    stmt = select(Customer.email).where(Customer.email == email)
+    result = session.execute(stmt).scalars().first()
+    return result is None # eu estou verificando se nao tem o email, entao se o result for None, quer dizer que nao tem o email no banco, 
 
-  def getCustomerInfo(self, email: str, session: Session = None) -> Customer:
+  @createSession
+  def getCustomerInfo(self, *, session: Session, email: str) -> Customer:
     stmt = select(Customer).where(Customer.email == email)
     customer = session.scalars(stmt).all()
     return customer[0]
